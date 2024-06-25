c849e1e019ad3cf6b3394d3fb7a0ba288c28475d
CarolHall-NCSU-CBE <95452316+CarolHall-NCSU-CBE@users.noreply.github.com>
2024-06-25 15:21:31 -0400

    Add files via upload
    Added revised version of Compare_AA_Frequency.py

--- PepBD_Analysis/Compare_AA_Frequency.py
+++ PepBD_Analysis/Compare_AA_Frequency.py
@@ -15,70 +15,17 @@ home = os.path.expanduser("~")
 #basic input files and parameters
 file = 'AminoAcidFrequency.csv'
 file_2 = 'AminoAcidPairFrequency.csv'
-pep_res = 10
-pol = 'PE'
-single_AA_plot = True
-pair_AA_plot = False
+pep_res = 10 #specify number of amino acids per peptide
+single_AA_plot = True #if True, then compare frequency of each amino acid
+pair_AA_plot = False #if True, then compare frequency of amino acid dimers
 
-## SPECIFY PATHS FOR COMPARISONS
-
-# paths = [f'/Users/Michael_1/PeptideDesigns/PE/QuantumCircuit',
-#          '/Users/Michael_1/Desktop/Temp/PepBD_Results/PE/MultipleStartStates']
-# names = ['Quantum Circuit', 'PepBD']
-
-# paths = [ '/Users/Michael_1/Desktop/Temp/PepBD_Results/PE/MultipleStartStates',
-#          '/Users/Michael_1/Desktop/Temp/PepBD_Results/PS/MultipleStartStates',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PE_not_PS_01Dec2023']
-# names = ['PepBD Designs: PE', 'PepBD Designs: PS', 'MCTS Designs: PE', 'MCTS Designs: PE vs. PS']
-
-paths = [ '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianhong_Designs/PaperDesigns_13Oct2023/CamSol_2.0',
-         '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PS/ML/Tianhong_Designs/PS_13Dec23',
-         '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianhong_Designs/PE_not_PS_01Dec2023',
-         '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PS/ML/Tianhong_Designs/PS_not_PE_11Dec23'
-]
-names = ['PE', 'PS', 'PE not PS', 'PS not PE']
-
-paths = [ '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/QA+RL/QA_2Body_01Dec23',
-         '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/QA+RL/QA+RL_GB+2Body_22Dec23',
-         '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/PepBD/20Structs_FixedBB_PepBD']
-names = ['QA', 'QA+RL', 'PepBD']
-
-paths = [ '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PET/VQC_Raul',
-         '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PepBD_Data/PepBD_Paper1/PET/SA+Hyd']
-names = ['VQC', 'PepBD']
-
-# paths = ['/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/QA+RL/QA+RL_GB+2Body_22Dec23',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/PepBD/20Structs_FixedBB_PepBD',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/QA+RL/RL_Match_PepBD_Score']
-# names = ['QA+RL', 'PepBD', 'RL, Score Matching']
-
-# paths = ['/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/CamSol_1.0',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/CamSol_2.0',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/CamSol_5.0',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/CamSol_10.0',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/CamSol_20.0']
-# names = ['SF=1.0', 'SF=2.0', 'SF=5.0', 'SF=10.0', 'SF=20.0']
-
-# paths = ['/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PepBD_Data/PepBD_Paper1/PE/SA+Hyd',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/Nominal',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/constrained']
-# names = ['PepBD', 'MCTS', 'MCTS Constrained']
-
-# paths = ['/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/ttH_0.01',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/ttH_0.1',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/ttH_0.3',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/ttH_0.5',
-#          '/Users/Michael_1/Documents/Grad_School/HallGroup/MicroplasticsProject/PeptideDesign/PE/ML/Tianong_Designs/PaperDesigns_13Oct2023/ttH_1.0']
-# names = ['SF=0.01', 'SF=0.1', 'SF=0.3', 'SF=0.5', 'SF=1.0']
-
+# specify paths to amino acid frequency files, as well as names for the runs
 paths = ['/Users/Michael_1/']
 names = ['dummy']
 num_names = len(names)
 
 #plotting parameters
 plt.rcdefaults()
-#plt.style.use(f'{home}/Python/presentation.mplstyle') #this is my custom format file
 bbox_loc = [0.5, -0.2]
 colors = plt.get_cmap('Pastel2').colors
 alpha=1
@@ -164,8 +111,10 @@ def plot_pair_AA_frequency(paths, cutoff_freq = 0.025):
     
     ax.legend(bbox_to_anchor=bbox_loc, loc='upper center', ncol=ncol, fontsize=legend_fontsize)
     fig.tight_layout()
-
-if single_AA_plot:
-    plot_single_AA_frequency(paths, pep_res)
-if pair_AA_plot:
-    plot_pair_AA_frequency(paths, pep_res)
+    
+    
+if __name__ == '__main__':
+    if single_AA_plot:
+        plot_single_AA_frequency(paths, pep_res)
+    if pair_AA_plot:
+        plot_pair_AA_frequency(paths, pep_res)
